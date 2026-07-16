9ffdad812e8312f326a885970a186da94d56c87f
Vladimir Rech <rechvladimir@hotmail.com>
2026-07-16 16:37:57 -0300

    Script para criar a tabela IBGE_MUNICIPIOS

---
+++ docker_oracle/cria_tabela.sql
@@ -0,0 +1,13 @@
+--
+-- Cria a tabela IBGE_MUNICIPIOS com os dados do IBGE
+--
+create table IBGE_MUNICIPIOS (
+    UF varchar2(2) not null,
+    COD_UF varchar(2) not null,
+    CODIGO_MUNICIPIO number(5) not null,
+    NOME_MUNICIPIO varchar2(100) not null,
+    POP_COLETADA number(10) not null,
+    POP_IMPUTADA number(5) not null,
+    POP_TOTAL number(10) not null,
+    PRIMARY KEY (CODIGO_MUNICIPIO)
+);
