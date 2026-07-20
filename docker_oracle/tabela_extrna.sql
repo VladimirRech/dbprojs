fb175fe399f5f048bce3e24e689ce524646b7005
Vladimir Rech <rechvladimir@hotmail.com>
2026-07-20 19:14:43 -0300

    Atualizando script

--- docker_oracle/tabela_extrna.sql
+++ docker_oracle/tabela_extrna.sql
@@ -2,7 +2,7 @@
 -- Configura pasta para a tabela externa
 --
 -- Cria o ponteiro para o diretório do servidor onde o arquivo de texto está salvo
-CREATE OR REPLACE DIRECTORY dir_importacao AS '/home/vlad/git/dbprojs/docker_oracle';
+CREATE OR REPLACE DIRECTORY dir_importacao AS 'oracle_data';
 
 -- Concede permissão de leitura para o usuário que vai importar
 GRANT READ, WRITE ON DIRECTORY dir_importacao TO sys;
